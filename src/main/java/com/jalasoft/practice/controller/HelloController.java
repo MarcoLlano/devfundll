fd43dc984714b2b55d4336a024d828e69b213d14
Marco Llano Gonzales <Marco Llano@jalasoft.local>
2020-05-03 13:17:14 -0400

    Solving copy file to inputFiles folder task

--- src/main/java/com/jalasoft/practice/controller/HelloController.java
+++ src/main/java/com/jalasoft/practice/controller/HelloController.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/hello")
@@ -14,8 +17,14 @@ public class HelloController {
     @PostMapping
     public String sayHello(@RequestParam(value="name") String name,
                            @RequestParam(value="lastName") String lastName,
-                           @RequestParam(value="file") File file) {
-        File returned = file;
+                           @RequestParam(value="file") MultipartFile file) {
+        File convertedFile = new File(System.getProperty("user.dir") + "/inputFiles/" + Objects.requireNonNull(file.getOriginalFilename()));
+        try {
+            file.transferTo(convertedFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return "Hello " + name + " " + lastName;
     }
 }
